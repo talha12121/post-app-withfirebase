c94d8c39279b3989ca4d041179735d1381bb5897
Talha Khan <tk411635@gmail.com>
2022-11-02 23:36:28 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,148 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
+
+// firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+
+import { 
+  doc, 
+  setDoc,
+  getFirestore, } 
+ from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+
+ import {
+   getStorage,
+   ref,
+   uploadBytesResumable,
+   getDownloadURL,
+  } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2Qk6buzReSnJfvo01Hsfw9DtHY_YqFY8",
+  authDomain: "new-post-62f4c.firebaseapp.com",
+  projectId: "new-post-62f4c",
+  storageBucket: "new-post-62f4c.appspot.com",
+  messagingSenderId: "447182263351",
+  appId: "1:447182263351:web:98589f9611290bdf9e9bb2",
+  measurementId: "G-5BYVLTTR3P"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth();
+  const db=getFirestore();
+
+  const submit = ()=>{
+    const name = document.getElementById("name");
+    const signup_email = document.getElementById("signup-email");
+    const signup_pass = document.getElementById("signup-pass");
+createUserWithEmailAndPassword(auth, signup_email.value, signup_pass.value)
+.then(async(userCredential) => { 
+
+  const uploadFiles = (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage();
+      let uid = auth.currentUser.uid;
+     
+      const storageRef = ref(storage, `users/${uid}.png`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+
+          if(progress==100){
+            swal("Account Successfully Created!", "", "success");
+          }
+          
+         
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              
+           
+              
+              break;
+          }
+        
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+  let myFile = document.getElementById("my-file");
+  let file = myFile.files[0];
+  
+  
+  let url = await uploadFiles(file);
+console.log(url)
+    const user = userCredential.user;
+      console.log("user",user)
+       
+      await setDoc(doc(db, "users", user.uid), {
+      name: name.value,
+      email: signup_email.value,
+      password: signup_pass.value,
+     url:url,
+    });   
+  })
+  .catch((error) => {
+     const errorCode = error.code;
+     const errorMessage = error.message;
+     console.log(errorMessage)
+  });
+
+  };
+
+const btn = document.getElementById("submit");
+btn.addEventListener("click", submit);
+
+
+
+
+    const login =()=>{
+    const login_email = document.getElementById("login-email");
+    const login_pass = document.getElementById("login-pass");
+    const auth = getAuth();
+signInWithEmailAndPassword(auth, login_email.value, login_pass.value)
+  .then(async(userCredential) => {
+    const user = userCredential.user;
+    console.log("user", user);
+    window.location="new.html"
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("error");
+    console.log(errorMessage)
+  });
+   }
+   const loginBtn = document.getElementById("login");
+   loginBtn.addEventListener("click", login);
+              
+   
+  
---
+++ new.js
@@ -0,0 +1,123 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
+import {
+  getAuth,
+  onAuthStateChanged, 
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { 
+    getDoc,
+     doc,
+     getDocs,
+     getFirestore,
+     collection,
+     query,
+     addDoc,
+     orderBy,
+     onSnapshot,
+    }  from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2Qk6buzReSnJfvo01Hsfw9DtHY_YqFY8",
+  authDomain: "new-post-62f4c.firebaseapp.com",
+  projectId: "new-post-62f4c",
+  storageBucket: "new-post-62f4c.appspot.com",
+  messagingSenderId: "447182263351",
+  appId: "1:447182263351:web:98589f9611290bdf9e9bb2",
+  measurementId: "G-5BYVLTTR3P"
+};
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth();
+  const db=getFirestore();
+  let textarea = document.getElementById("textarea");
+  let post = document.getElementById("postBtn");
+  let users= document.getElementById("all_users")
+  let new_post = document.getElementById("new_posts")
+
+
+  onAuthStateChanged(auth, async (user) => {
+      if (user) {
+          const uid = user.uid;
+          const docRef = doc(db, "users", uid);
+          const docSnap = await getDoc(docRef);  
+
+         
+          if (docSnap.exists()) {
+              console.log("Document data:", docSnap.data());
+               document.getElementById("img").innerHTML=`<img class="img1" src="${docSnap.data().url}">`
+               document.getElementById("img1").innerHTML=`<img class="img1" src="${docSnap.data().url}">`
+              document.getElementById("name").innerHTML=docSnap.data().name;
+              document.getElementById("userName").innerHTML=docSnap.data().name; 
+              
+              post.addEventListener("click",async()=>{
+                // Add a new document with a generated id.
+                const docRef = await addDoc(collection(db, "posts"), {
+                  name: docSnap.data().name,
+                  post : textarea.value,
+                  TimeStamp :new Date(),
+                  // url:docSnap.data().url,
+                  // url:url,
+                });
+                console.log("Document written with ID: ", docRef.id);
+                      
+                
+
+
+          })
+            } else {
+                console.log("No such document!");
+           }       
+        }})
+        
+
+        users.addEventListener("click",async()=>{
+          frndz.innerHTML=""
+            const q = query(collection(db, "users"));         
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+                let frndz = document.getElementById("frndz");
+                frndz.innerHTML+=`
+                <div> <img  src="${doc.data().url}" style="width:50px; height:50px; background-size:cover; border-radius:60%; border:2px solid gray;"  >
+          ${doc.data().name}</div>`
+          console.log(doc.data().name)
+    
+      });
+    })
+   
+      
+      // const q = query(collection(db, "posts"));
+      const q = query(collection(db, "posts"), orderBy("TimeStamp", "desc"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const cities = [];
+        new_post.innerHTML="";
+        
+        querySnapshot.forEach((doc) => {
+            cities.push(doc.data().name);
+            new_post.innerHTML+=`
+            <div class="js-post">
+            <div class="title">
+            <div class="js-sec">
+            <div class="time">        
+            
+            <p class="js-name">${doc.data().name}</p>
+            </div>
+            </div>
+            </div>
+            <div class="bgPosition">
+            <p>
+            ${doc.data().post}
+            </p>
+            </div>
+            </div>
+            
+            `
+          
+  textarea.value=""
+        });
+        console.log("Current cities in CA: ", cities.join(", "));
+      });
+    
+
+     
